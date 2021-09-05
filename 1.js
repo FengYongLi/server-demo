b2fd228a1d5dade315feff60117cd9394eef926a
LiYongFeng <506076395@qq.com>
2021-09-05 13:50:03 +0800

    user => users.json

--- 1.js
+++ 1.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
-// 读取数据
+
+// 读取数据库
 const usersString = fs.readFileSync('./db/users.json').toString()
 const usersArray = JSON.parse(usersString)
 
-console.log(typeof usersString)
-console.log(usersString)
-
-console.log(typeof usersArray)
-console.log(usersArray instanceof Array)
-console.log(usersArray)
+// 写数据库
+const user3 = {id:3,name:'tom',password:'ccc'}
+//写的新数据放到数组数据中
+usersArray.push(user3)
+// 把符合json 语法的数组转为字符串
+const string = JSON.stringify(usersArray)
+// 读取更新后的数据库
+fs.writeFileSync('./db/users.json', string)
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ node-dev server.js 8888
  try catch
 # 3. 默认首页
  默认根目录 / 跳转首页
-# 4. 修改 Content-Type
-# 5. 读取数据库
+# 4. 修改 Content-Type 静态服务器结束
+# 5. 动态服务器开始：读取数据库
  node 1.js
+# 6. 数据库写入数据
+ user => users.json
