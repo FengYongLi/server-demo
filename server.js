aee9af81e18a2ba3378ef3cfbb083c2ef06fba8c
LiYongFeng <506076395@qq.com>
2021-09-05 15:48:50 +0800

    用户登录

--- server.js
+++ server.js
@@ -20,7 +20,35 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-  if (path === "/login" && method === "POST") {
+  if(path === "/sign_in" && method === "POST"){
+    // 需要先读取数据库 要看下你的用户名和密码在不在数据库
+    const userArray = JSON.parse(fs.readFileSync('./db/users.json'))
+    // 由于不确认数据长度大小，需要批次上传，声明一个空数组来获取批次内容
+    const array = []
+    request.on('data', (chunk) => {
+      array.push(chunk)
+    })
+    request.on('end', () => {
+      // 用 Buffer.concat() 把数据链接为字符串
+      const string = Buffer.concat(array).toString()
+      // 转为对象 这个对象有 post 来的 数据
+      const obj = JSON.parse(string)
+      // 这里就不需要 lastUser 了 这里只需要看我的数据库中的 name 和 password 有没有和你一样的
+      // 可以用 find 查找当前用户(user)的 name 和 password 是不是跟 数据库里的匹配
+      const user = userArray.find((user)=> user.name === obj.name && user.password === obj.password)
+      if(user === undefined){
+        response.statusCode = 400
+        response.setHeader('content-Type', 'text/json; charset=utf-8')
+        response.end(`{"errorCode": 401}`)
+      }else{
+        response.statusCode = 200
+        response.end()
+      }
+    })
+  }else if (path === "/home.html") {
+    // 写出来 不知道当前用户是谁
+    response.end('hi') // 这里必须有 end 没有跳转过去会一直请求等结果
+}else if (path === "/login" && method === "POST") {
     response.setHeader('content-Type', 'text/html; charset=utf-8')
     // 需要先读取数据库 下边查找下标最大的会用到
     const userArray = JSON.parse(fs.readFileSync('./db/users.json'))
